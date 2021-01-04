cddaab2d65b0cd0eb8fcb49ad0224e32d4a732f1
ous8292 <35242265+ous8292@users.noreply.github.com>
2021-01-04 09:27:56 -0600

    Update RPSgameV3withBot.py
    Cleaned code up

--- RPSgameV3withBot.py
+++ RPSgameV3withBot.py
@@ -1,55 +1,61 @@
 
 
 
-##print("Rock")
-##print("Paper")
-##print("scissors")
-##
+
 
 ## MY SOLUTION
-#player1 = input("Player 1, make your move: ")
 
-##import random
-##
-##bot_choice = random.randint(0,2)
-##if bot_choice == 0:
-##    print('Bot chooses Rock')
-##elif bot_choice == 1:
-##    print('Bot chooses Paper')
-##elif bot_choice == 2:
-##    print('Bot chooses scissors!')
-##else:
-##    print('I broke')
-##
-##
-##if player1 == bot_choice: 
-##    print("It's a tie!") 
-##elif player1 == 'rock':
-##    if bot_choice == 1:
-##        print('bot wins!')
-##    elif bot_choice == 2:
-##        print('player 1 wins!')
-##elif player1 == 'paper':
-##    if bot_choice == 0:
-##        print('player 1 wins!')
-##    elif bot_choice == 2:
-##        print('bot wins!')
-##elif player1 == 'scissor':
-##    if bot_choice == 0:
-##        print('bot wins!')
-##    elif bot_choice == 2:
-##        print('player 1 wins!')
-##else:
-##    print("Something went terribly wrong")
-##
-##
 
 #randint needs to be imported
+import random
+
+print("Rock")
+print("Paper")
+print("scissors")
+
+player1 = input("Player 1, make your move: ") #player input
+
 #randint generates random number 0 - 2
-
 # conditional that says if 0 pick rock, if 1 pick paper if, 2 pick scissor
+bot_choice = random.randint(0,2)
+if bot_choice == 0:
+    print('Bot chooses Rock')
+elif bot_choice == 1:
+    print('Bot chooses Paper')
+elif bot_choice == 2:
+    print('Bot chooses scissors!')
+else:
+    print('I broke') #not nescessary, I just put it here incase of errors.
 
 
+
+if player1 == bot_choice: 
+    print("It's a tie!") 
+elif player1 == 'rock':
+    if bot_choice == 1:
+        print('bot wins!')
+    elif bot_choice == 2:
+        print('player 1 wins!')
+elif player1 == 'paper':
+    if bot_choice == 0:
+        print('player 1 wins!')
+    elif bot_choice == 2:
+        print('bot wins!')
+elif player1 == 'scissor':
+    if bot_choice == 0:
+        print('bot wins!')
+    elif bot_choice == 2:
+        print('player 1 wins!')
+else:
+    print("Something went terribly wrong")
+
+
+
+
+
+
+
+"""
 #COURSE SOLUTION
 import random
 
@@ -85,7 +91,7 @@ elif player == "scissors":
 
 
 
-
+"""
 
 
 
