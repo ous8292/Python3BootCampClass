624fc969c2fded9c767157b0932c4adfb493c497
ous8292 <35242265+ous8292@users.noreply.github.com>
2021-01-03 21:02:52 -0600

    Add files via upload
    rock paper scissors, except it has a computer player using randint and if/else statements

---
+++ RPSgameV3withBot.py
@@ -0,0 +1,94 @@
+
+
+
+##print("Rock")
+##print("Paper")
+##print("scissors")
+##
+
+## MY SOLUTION
+#player1 = input("Player 1, make your move: ")
+
+##import random
+##
+##bot_choice = random.randint(0,2)
+##if bot_choice == 0:
+##    print('Bot chooses Rock')
+##elif bot_choice == 1:
+##    print('Bot chooses Paper')
+##elif bot_choice == 2:
+##    print('Bot chooses scissors!')
+##else:
+##    print('I broke')
+##
+##
+##if player1 == bot_choice: 
+##    print("It's a tie!") 
+##elif player1 == 'rock':
+##    if bot_choice == 1:
+##        print('bot wins!')
+##    elif bot_choice == 2:
+##        print('player 1 wins!')
+##elif player1 == 'paper':
+##    if bot_choice == 0:
+##        print('player 1 wins!')
+##    elif bot_choice == 2:
+##        print('bot wins!')
+##elif player1 == 'scissor':
+##    if bot_choice == 0:
+##        print('bot wins!')
+##    elif bot_choice == 2:
+##        print('player 1 wins!')
+##else:
+##    print("Something went terribly wrong")
+##
+##
+
+#randint needs to be imported
+#randint generates random number 0 - 2
+
+# conditional that says if 0 pick rock, if 1 pick paper if, 2 pick scissor
+
+
+#COURSE SOLUTION
+import random
+
+player = input("Player 1, make your move: ")
+
+rand_num = random.randint(0, 2)
+if rand_num == 0:
+    computer = "rock"
+elif rand_num == 1:
+    computer = "paper"
+else:
+    computer = "scissors"
+print(computer)
+
+
+if player == computer: 
+    print("It's a tie!") 
+elif player == "rock":
+    if computer == "scissors":
+        print("player wins!")
+    elif computer == "paper":
+            print("computer wins")
+elif player == "paper":
+    if computer == "rock":
+        print("player wins!")
+    elif computer == "scissors":
+            print("computer wins")
+elif player == "scissors":
+    if computer == "paper":
+        print("player wins!")
+    elif computer == "rock":
+            print("computer wins")
+
+
+
+
+
+
+
+
+
+
